312f8f8451d976d83a7dd8912cab63d5b728befe
Park-Jongchan <rachachany@naver.com>
2024-12-14 13:56:15 +0900

    feat: 대부분의 기능 완성, 리뷰 반영, 디자인 규격 반영, 버튼 보류

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 // Form 데이터 타입 정의
 interface LoginFormInputs {
@@ -13,6 +14,7 @@ interface LoginFormInputs {
 const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
 function LoginPage() {
+  const [showModal, setShowModal] = useState(false); // 모달 상태 관리
   const router = useRouter(); // useRouter 선언
 
   const {
@@ -35,79 +37,107 @@ function LoginPage() {
       alert('로그인 성공!');
       router.push('/posts'); // 공고 리스트 페이지로 이동
     } else {
-      alert('비밀번호가 일치하지 않습니다.');
+      setShowModal(true); // 비밀번호 불일치 시 모달 표시
     }
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-md">
-        {/* 로고 버튼 */}
-        <img
-          src="/logo.png"
-          alt="Logo"
-          className="mx-auto mb-8 h-12 cursor-pointer"
-          onClick={() => router.push('/posts')} // 공고 리스트 페이지로 이동
-        />
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {/* 이메일 입력 */}
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-            이메일
-          </label>
-          <input
-            id="email"
-            type="email"
-            {...register('email', {
-              required: '이메일을 입력해 주세요.',
-              pattern: {
-                value: emailRegex,
-                message: '이메일 형식으로 작성해 주세요.',
-              },
-            })}
-            onBlur={() => trigger('email')} // 포커스 아웃 시 유효성 검사 실행
-            className={`mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 ${
-              errors.email ? 'border-red-500' : ''
-            }`}
-            placeholder="이메일 입력"
-          />
-          {errors.email && <p className="mt-1 text-sm text-red-500">{errors.email?.message}</p>}
+    <div className="flex min-h-screen items-center justify-center bg-gray-white font-sans">
+      {/* 모달 */}
+      {showModal && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-black bg-opacity-50">
+          <div className="w-full max-w-[540px] rounded-lg bg-gray-white p-6 shadow-lg sm:mx-6 md:mx-8 lg:mx-10">
+            <h2 className="mb-24 mt-24 text-center text-lg" style={{ color: '#333236' }}>
+              비밀번호가 일치하지 않습니다.
+            </h2>
+            <div className="flex justify-end">
+              <button
+                onClick={() => setShowModal(false)} // 모달 닫기
+                className="w-28 rounded-md py-2 text-gray-white"
+                style={{ backgroundColor: '#ea3c12' }}
+              >
+                확인
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
 
-          {/* 비밀번호 입력 */}
-          <label htmlFor="password" className="mt-4 block text-sm font-medium text-gray-700">
-            비밀번호
-          </label>
-          <input
-            id="password"
-            type="password"
-            {...register('password', {
-              required: '비밀번호를 입력해 주세요.',
-              minLength: {
-                value: 8,
-                message: '8자 이상 작성해 주세요.',
-              },
-            })}
-            onBlur={() => trigger('password')} // 포커스 아웃 시 유효성 검사 실행
-            className={`mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 ${
-              errors.password ? 'border-red-500' : ''
-            }`}
-            placeholder="비밀번호 입력"
-          />
-          {errors.password && (
-            <p className="mt-1 text-sm text-red-500">{errors.password?.message}</p>
-          )}
+      <div className="w-full max-w-md rounded-lg bg-gray-white p-8">
+        <Image
+          src="/images/logo.svg"
+          alt="Logo"
+          width={248}
+          height={45}
+          className="mx-auto mb-8 cursor-pointer"
+          onClick={() => router.push('/posts')}
+        />
+
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-7">
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="email"
+              className="decoration-skip-ink-none text-left font-sans text-base font-normal leading-[26px] text-gray-black underline-offset-auto"
+            >
+              이메일
+            </label>
+            <input
+              id="email"
+              type="email"
+              {...register('email', {
+                required: '이메일을 입력해 주세요.',
+                pattern: {
+                  value: emailRegex,
+                  message: '이메일 형식으로 작성해 주세요.',
+                },
+              })}
+              onBlur={() => trigger('email')}
+              className={`w-full rounded-md border px-4 py-2 text-sm`}
+              placeholder="입력"
+            />
+            {errors.email && <p className="mt-1 text-sm text-red-40">{errors.email?.message}</p>}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="password"
+              className="decoration-skip-ink-none text-left font-sans text-base font-normal leading-[26px] text-gray-black first-letter:underline-offset-auto"
+            >
+              비밀번호
+            </label>
+            <input
+              id="password"
+              type="password"
+              {...register('password', {
+                required: '비밀번호를 입력해 주세요.',
+                minLength: {
+                  value: 8,
+                  message: '8자 이상 작성해 주세요.',
+                },
+              })}
+              onBlur={() => trigger('password')}
+              className={`w-full rounded-md border px-4 py-2 text-sm`}
+              placeholder="입력"
+            />
+            {errors.password && (
+              <p className="mt-1 text-sm text-red-40">{errors.password?.message}</p>
+            )}
+          </div>
 
           <button
             type="submit"
-            className="mt-4 w-full rounded-md bg-red-500 py-2 font-semibold text-white hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300"
+            className="mt-4 w-full rounded-md bg-red-40 py-2 font-semibold text-white hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300"
           >
             로그인 하기
           </button>
         </form>
-        <p className="mt-6 text-center text-sm text-gray-500">
+
+        <p className="mt-6 text-center font-sans" style={{ color: '#333236' }}>
           회원이 아니신가요?{' '}
           <span
             onClick={() => router.push('/signup')} // 회원가입 페이지로 이동
-            className="cursor-pointer text-blue-500 hover:underline"
+            className="cursor-pointer text-blue-500 underline"
+            style={{ color: '#5534da' }}
           >
             회원가입하기
           </span>
