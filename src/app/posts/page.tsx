bac3815e1b6c11bf5d12ae8d88ed95c1f98998ae
1022gusl <1022gusl@gmail.com>
2024-12-15 16:31:44 +0900

    feat(dropdown): add  postdropdown

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,12 @@
+import PostDropdown from '../components/PostDropdown';
+
+export default function Posts() {
+  return (
+    <div className="ml-4">
+      드롭다운 테스트 중입니다
+      <div>
+        <PostDropdown />
+      </div>
+    </div>
+  );
+}
