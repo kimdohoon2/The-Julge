8116bdcf6e019e8d4b0b1942382fdc97fab59c5a
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2024-12-16 11:28:58 +0900

    feat(ui) : 코드리뷰 반영

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,18 +1,19 @@
 import Link from 'next/link';
+
 export default function MyShopPage() {
   return (
-    <section className="w-full px-[12px] pt-[142px] md:px-[32px] md:pt-[130px] xl:px-[237px]">
-      <h3 className="mb-[16px] text-[20px] font-bold md:mb-[24px] md:text-[28px]">내 가게</h3>
-      <div className="w-full rounded-[12px] border border-gray-20">
-        <div className="flex flex-col items-center gap-[16px] px-[24px] py-[60px] md:gap-[24px]">
-          <p className="text-[14px] font-[400] md:text-[16px]">
+    <section className="w-full px-3 pt-[8.875rem] md:px-8 md:pt-[8.125rem] xl:px-[14.8125rem]">
+      <h3 className="mb-4 text-lg font-bold md:mb-6 md:text-xl">내 가게</h3>
+      <div className="w-full rounded-lg border border-gray-20">
+        <div className="flex flex-col items-center gap-4 px-6 py-[3.75rem] md:gap-6">
+          <p className="text-sm font-normal text-gray-black md:text-base">
             내 가게를 소개하고 공고도 등록해 보세요.
           </p>
           <Link
-            className="bg-orange flex h-[37px] w-full max-w-[108px] items-center justify-center rounded-[6px] md:h-[47px] md:max-w-[346px]"
-            href={'/register'}
+            className="bg-orange flex h-[2.3125rem] w-full max-w-[6.75rem] items-center justify-center rounded-md md:h-[2.9375rem] md:max-w-[21.625rem]"
+            href='/register'
           >
-            <span className="text-[14px] text-gray-white md:text-[16px]">가게 등록하기</span>
+            <span className="text-sm text-gray-white md:text-base">가게 등록하기</span>
           </Link>
         </div>
       </div>
