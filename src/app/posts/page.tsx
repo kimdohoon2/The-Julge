1d6467d26648020ec5c18ea11296f0eb6f7dfc91
1022gusl <1022gusl@gmail.com>
2024-12-16 16:41:40 +0900

    feat(pagination): 공고 페이지네이션 버튼 추가

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,15 +1,11 @@
-import CardList from '../components/CardList';
-import DetailedFilter from '../components/DetailedFilter';
-import PostDropdown from '../components/PostDropdown';
+import Pagination from '../components/Pagination';
 
 export default function Posts() {
+  const totalItems = 132; // 제가 임시로 지정해둔 전체 아이템 수 입니다
+  const itemsPerPage = 6; // 제가 임시로 지정해둔 한 번에 보여줄 아이템 수 입니다
   return (
-    <div className="ml-4">
-      <CardList />
-      <div>
-        <PostDropdown />
-        <DetailedFilter />
-      </div>
+    <div className="m-4">
+      <Pagination totalItems={totalItems} itemsPerPage={itemsPerPage} />
     </div>
   );
 }
