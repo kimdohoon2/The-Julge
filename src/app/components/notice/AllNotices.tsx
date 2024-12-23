8db60c01627005bb54704935520a4462b48b0da2
1022gusl <1022gusl@gmail.com>
2024-12-23 23:41:30 +0900

    feat(filter): 필터링 후 데이터가 없을 시 사용자 경험 개선을 위해 간단한 문구 추가

--- src/app/components/notice/AllNotices.tsx
+++ src/app/components/notice/AllNotices.tsx
@@ -79,30 +79,39 @@ export default function AllNotices({
 
   return (
     <div className="grid grid-cols-2 gap-4 lg:grid-cols-3">
-      {notices.map((notice) => {
-        const increaseRate = (
-          ((notice.hourlyPay - notice.shop.item.originalHourlyPay) /
-            notice.shop.item.originalHourlyPay) *
-          100
-        ).toFixed(0);
+      {notices.length > 0 ? (
+        notices.map((notice) => {
+          const increaseRate = (
+            ((notice.hourlyPay - notice.shop.item.originalHourlyPay) /
+              notice.shop.item.originalHourlyPay) *
+            100
+          ).toFixed(0);
 
-        return (
-          <div key={notice.id} className="w-44 sm:w-[312px]">
-            <Card
-              image={notice.shop.item.imageUrl}
-              title={notice.shop.item.name}
-              date={notice.startsAt.split('T')[0]}
-              hours={formatTimeRange(notice.startsAt, notice.workhour)}
-              location={notice.shop.item.address1}
-              price={`${notice.hourlyPay.toLocaleString()}원`}
-              discount={parseFloat(increaseRate) > 0 ? `기존 시급보다 ${increaseRate}%` : undefined}
-              noticeId={notice.id}
-              shopId={notice.shopId}
-              onClick={() => addNotice(notice)}
-            />
-          </div>
-        );
-      })}
+          return (
+            <div key={notice.id} className="w-44 sm:w-[312px]">
+              <Card
+                image={notice.shop.item.imageUrl}
+                title={notice.shop.item.name}
+                date={notice.startsAt.split('T')[0]}
+                hours={formatTimeRange(notice.startsAt, notice.workhour)}
+                location={notice.shop.item.address1}
+                price={`${notice.hourlyPay.toLocaleString()}원`}
+                discount={
+                  parseFloat(increaseRate) > 0 ? `기존 시급보다 ${increaseRate}%` : undefined
+                }
+                noticeId={notice.id}
+                shopId={notice.shopId}
+                onClick={() => addNotice(notice)}
+              />
+            </div>
+          );
+        })
+      ) : (
+        <div className="col-span-2 flex flex-col items-center justify-center lg:col-span-3">
+          <p className="text-lg text-gray-black">해당 조건에 맞는 공고가 없습니다.</p>
+          <p className="text-sm text-orange">필터를 다시 설정해보세요!</p>
+        </div>
+      )}
     </div>
   );
 }
