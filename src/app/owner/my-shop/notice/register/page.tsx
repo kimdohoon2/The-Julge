72f6d74a2e58bf131f4721bd6e4704e15cd36f5e
JUYOUNG\vilio <vilioite@naver.com>
2024-12-24 10:41:44 +0900

    refactor: 필요없는 props 제거

--- src/app/owner/my-shop/notice/register/page.tsx
+++ src/app/owner/my-shop/notice/register/page.tsx
@@ -3,12 +3,10 @@
 import NoticeRegisterForm from '@/app/components/my-shop/register/NoticeRegisterForm';
 import useAuthStore from '@/app/stores/authStore';
 import { postShopNotice } from '@/app/api/api';
-import { useParams } from 'next/navigation';
 
 export default function NoticeRegisterEditPage() {
   const { token, user } = useAuthStore();
   const shopId = user?.shop?.item.id;
-  const { id } = useParams<{ id: string }>();
 
   if (!shopId || !token) {
     return <div className="my-10 text-center">로그인이 필요합니다.</div>;
@@ -20,7 +18,6 @@ export default function NoticeRegisterEditPage() {
         mode="create"
         token={token}
         shopId={shopId}
-        noticeId={id as string}
         createApi={(token, shopId, data) => postShopNotice(token, shopId, data)}
       />
     </>
