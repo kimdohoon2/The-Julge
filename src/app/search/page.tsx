0a9bdfad77ebfbeddc387353d8d4081a11173e85
kimdohoon2 <kingyska@nate.com>
2024-12-27 04:51:11 +0000

    fix(search): 검색 suspense 추가

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import AllNotices from '../components/notice/AllNotices';
 import DetailedFilter from '../components/notice/DetailedFilter';
@@ -10,7 +10,7 @@ import formatSortToApi from '../utils/formatSortToApi';
 
 const container = 'mx-auto px-4 sm:px-8 lg:px-0 pb-4 max-w-[964px] pt-10 sm:pb-4 lg:pb-14';
 
-export default function Search() {
+export default function SearchPage() {
   const searchParams = useSearchParams();
   const query = searchParams.get('q') || '';
 
@@ -40,38 +40,40 @@ export default function Search() {
   };
 
   return (
-    <div>
-      <div className={`${container} mb-8 flex min-h-[300px] flex-col lg:mb-0`}>
-        <div className={`mb-5 sm:flex sm:items-center sm:justify-between`}>
-          <h2 className="text-xl font-bold text-gray-black sm:text-[28px]">
-            <span className="text-orange">{query}</span>에 대한 공고 목록
-          </h2>
-          <div className="mt-4 flex items-center gap-3 sm:mt-0">
-            <NoticeDropdown
-              onChange={(selectedSort) => {
-                setSortOption(selectedSort);
-                setCurrentPage(1);
-              }}
-            />
-            <DetailedFilter onFilterChange={handleFilterChange} />
+    <Suspense fallback={null}>
+      <div>
+        <div className={`${container} mb-8 flex min-h-[300px] flex-col lg:mb-0`}>
+          <div className={`mb-5 sm:flex sm:items-center sm:justify-between`}>
+            <h2 className="text-xl font-bold text-gray-black sm:text-[28px]">
+              <span className="text-orange">{query}</span>에 대한 공고 목록
+            </h2>
+            <div className="mt-4 flex items-center gap-3 sm:mt-0">
+              <NoticeDropdown
+                onChange={(selectedSort) => {
+                  setSortOption(selectedSort);
+                  setCurrentPage(1);
+                }}
+              />
+              <DetailedFilter onFilterChange={handleFilterChange} />
+            </div>
           </div>
+          <AllNotices
+            currentPage={currentPage}
+            itemsPerPage={itemsPerPage}
+            setTotalItems={setTotalItems}
+            sortOption={formatSortToApi(sortOption)}
+            filterOptions={filterOptions}
+            query={query}
+          />
         </div>
-        <AllNotices
-          currentPage={currentPage}
+        <Pagination
+          totalItems={totalItems}
           itemsPerPage={itemsPerPage}
-          setTotalItems={setTotalItems}
-          sortOption={formatSortToApi(sortOption)}
-          filterOptions={filterOptions}
-          query={query}
+          currentPage={currentPage}
+          onPageChange={setCurrentPage}
+          className="mb-16"
         />
       </div>
-      <Pagination
-        totalItems={totalItems}
-        itemsPerPage={itemsPerPage}
-        currentPage={currentPage}
-        onPageChange={setCurrentPage}
-        className="mb-16"
-      />
-    </div>
+    </Suspense>
   );
 }
